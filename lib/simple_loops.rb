a79b0d814a3890d8fbad2f43ebdaec89af95ba1c
Alex Marz <alexhmarz@gmail.com>
2020-10-31 22:10:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,11 +1,5 @@
 # Write your methods here
 
 def loop_message_five_times
-  counter = 1
-  while counter <= 5 do
-    puts "Hello World."
-    counter += 1 
-  end
-end
-
+  puts "Hello World."
   
